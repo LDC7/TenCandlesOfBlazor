4faf0f7a0a19eb372fae974a4be7efc069eeb30d
LDC7 <lihwar1@gmail.com>
2024-03-02 10:12:13 +0400

    init sln

---
+++ TenCandlesOfBlazor/Data/WeatherForecast.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TenCandlesOfBlazor.Data;
+
+public class WeatherForecast
+{
+  public DateTime Date { get; set; }
+
+  public int TemperatureC { get; set; }
+
+  public int TemperatureF => 32 + (int)(this.TemperatureC / 0.5556);
+
+  public string? Summary { get; set; }
+}
